85aaefb1146b3a89799932290c47563429b01996
AntonHlushchenko <glantoshka@gmail.com>
2023-06-09 20:41:37 +0300

    dashboard part 1

--- src/route/index.js
+++ src/route/index.js
@@ -17,6 +17,12 @@ router.get('/slack', function (req, res) {
 	})
 })
 
+router.get('/dashboard', function (req, res) {
+	res.render('dashboard', {
+		layout: null,
+	})
+})
+
 router.get('/template-1', function (req, res) {
 	res.render('template-1', {
 		layout: 'template-1',
